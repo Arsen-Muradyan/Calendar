6eda4e0fd85afcc8e8ccd6b475af9e0627656a0f
Arsen-Muradyan <arsenmuradyan604@gmail.com>
2020-01-18 20:18:16 +0400

    Initial Commit

--- js/script.js
+++ js/script.js
@@ -1,26 +1,39 @@
 // Declare Vairbales
-var daysBox = document.getElementsByClassName('days')
+var daysBox = document.getElementsByClassName('days');
 var monthTitle = document.getElementsByClassName('month-title');
 var yearTitle = document.getElementsByClassName('year-title');
-var monDays = document.getElementById("mon-days");
-var tueDays = document.getElementById("tue-days");
-var wedDays = document.getElementById("wed-days");
-var thuDays = document.getElementById("thu-days");
-var friDays = document.getElementById("fri-days");
-var satDays = document.getElementById("sat-days");
-var sunDays = document.getElementById("sun-days");
+var monDays = document.getElementById('mon-days');
+var tueDays = document.getElementById('tue-days');
+var wedDays = document.getElementById('wed-days');
+var thuDays = document.getElementById('thu-days');
+var friDays = document.getElementById('fri-days');
+var satDays = document.getElementById('sat-days');
+var sunDays = document.getElementById('sun-days');
 var btnNext = document.getElementsByClassName('btn-next');
 var btnPrev = document.getElementsByClassName('btn-prev');
 // Add Event
-document.addEventListener("DOMContentLoaded", start);
+document.addEventListener('DOMContentLoaded', start);
 // Start Function
 function start() {
   // Create Date Object
   var d = new Date();
   // Month Names Array
-  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October","November", "December"];
+  var months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
   //Days Array
-  var days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+  var days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
   // Setup Date data
   var year = d.getFullYear();
   var month = d.getMonth();
@@ -36,7 +49,7 @@ function start() {
     month++;
     if (month > 11) {
       month = 0;
-      year++
+      year++;
     }
     monthTitle[0].innerHTML = months[month];
     yearTitle[0].innerHTML = year;
@@ -47,13 +60,12 @@ function start() {
     month--;
     if (month < 0) {
       month = 11;
-      year--
-    } 
+      year--;
+    }
     monthTitle[0].innerHTML = months[month];
     yearTitle[0].innerHTML = year;
     dropDays();
     createDays();
-
   }
   // Clear Calendar Data Function
   function dropDays() {
@@ -67,7 +79,11 @@ function start() {
   }
   // Create Calendar Data function
   function createDays() {
-    for (var i = -(getFirstDay(year, month) - 1); i <= dayInMonth(d.getFullYear(), d.getMonth() + 1); i++) {
+    for (
+      var i = -(getFirstDay(year, month) - 1);
+      i <= dayInMonth(d.getFullYear(), d.getMonth() + 1);
+      i++
+    ) {
       // Set Year, Month, Day
       d.setFullYear(year, month, i);
       // Add Calendar Counts
@@ -75,22 +91,25 @@ function start() {
       // Check special days
       if (i < 1) {
         txt = `<span class="text-old">${d.getDate()}</span><br><br>`;
-      }
-      else if (days[currentDay(d.getDay())].toLowerCase() == 'sat' ||
-        days[currentDay(d.getDay())].toLowerCase() == 'sun') {
+      } else if (
+        days[currentDay(d.getDay())].toLowerCase() == 'sat' ||
+        days[currentDay(d.getDay())].toLowerCase() == 'sun'
+      ) {
         txt = `<span class="text-holiday">${d.getDate()}</span><br><br>`;
-        if (currentDate().day == d.getDate() &&
+        if (
+          currentDate().day == d.getDate() &&
           currentDate().month == d.getMonth() &&
-          currentDate().year == d.getFullYear()) {
+          currentDate().year == d.getFullYear()
+        ) {
           txt = `<span class="text-active">${d.getDate()}</span><br><br>`;
         }
-      }
-      else if (currentDate().day == d.getDate() &&
+      } else if (
+        currentDate().day == d.getDate() &&
         currentDate().month == d.getMonth() &&
-        currentDate().year == d.getFullYear()) {
+        currentDate().year == d.getFullYear()
+      ) {
         txt = `<span class="text-active">${d.getDate()}</span><br><br>`;
-      }
-      else {
+      } else {
         txt = `<span>${d.getDate()}</span><br><br>`;
       }
       // Check Date Day
@@ -132,12 +151,12 @@ function getFirstDay(y, m) {
   var d = new Date();
   d.setFullYear(y, m, 1);
   return currentDay(d.getDay());
-} 
+}
 function currentDate() {
   var d = new Date();
   return {
     year: d.getFullYear(),
     month: d.getMonth(),
-    day: d.getDate(),
+    day: d.getDate()
   };
 }
